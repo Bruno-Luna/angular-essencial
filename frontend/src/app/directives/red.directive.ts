fcef8d22432437d91f3c21d110261e3ec66f9db5
Bruno Luna <luna.brsilva@gmail.com>
2021-11-15 11:39:25 -0300

    [05] - Diretivas

---
+++ frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appRed]'
+})
+export class RedDirective {
+
+  constructor(
+    private el: ElementRef
+  ) { 
+    el.nativeElement.style.color = '#ff3333'
+  }
+
+}
