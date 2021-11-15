1e209d8fc633c8acd25029a5c914b7c7181153cc
Bruno Luna <luna.brsilva@gmail.com>
2021-11-15 11:46:57 -0300

    [06] - Criação do Componente product-create e inserção da rota

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -8,7 +9,8 @@ import {ProductCrudComponent} from './views/product-crud/product-crud.component'
 const routes: Routes = [
 
   {path: "", component: HomeComponent},
-  {path: "products", component: ProductCrudComponent}
+  {path: "products", component: ProductCrudComponent},
+  {path: "products/create", component: ProductCreateComponent}
 
 ];
 
