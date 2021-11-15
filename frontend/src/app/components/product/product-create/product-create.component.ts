f0e09388afbb1b4de48607d61951be9b6e78f146
Bruno Luna <luna.brsilva@gmail.com>
2021-11-15 12:16:18 -0300

    [07] - EventBindings para navegar entre componentes

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+
+  telefonema = "Alô"
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fazerAlgo(){
+    console.log('Oláááááá');
+    
+  }
 }
