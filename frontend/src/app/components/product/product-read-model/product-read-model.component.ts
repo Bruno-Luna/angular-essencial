c209fc2eb41e3a5788d8dc314bb61abb82e423d6
Bruno Luna <luna.brsilva@gmail.com>
2021-11-17 10:08:21 -0300

    [11] - Componente read-model e inserção da tabela a partir do Table Schematic

---
+++ frontend/src/app/components/product/product-read-model/product-read-model.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ProductReadModelDataSource, ProductReadModelItem } from './product-read-model-datasource';
+
+@Component({
+  selector: 'app-product-read-model',
+  templateUrl: './product-read-model.component.html',
+  styleUrls: ['./product-read-model.component.css']
+})
+export class ProductReadModelComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ProductReadModelItem>;
+  dataSource: ProductReadModelDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new ProductReadModelDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
