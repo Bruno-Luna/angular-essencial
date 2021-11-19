53aa126d0efab80654111b710d939cc54d083549
Bruno Luna <luna.brsilva@gmail.com>
2021-11-19 08:46:33 -0300

    [13] - Criação dos métodos readById e update

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -30,4 +30,13 @@ export class ProductService {
   read(): Observable<Product[]>{
     return this.http.get<Product[]>(this.baseUrl)
   }
+
+  readById(id: string): Observable<Product>{
+    return this.http.get<Product>(`this.baseUrl/${id}`)
+  }
+
+  update(product: Product): Observable<Product>{
+    return this.http.put<Product>(`this.baseUrl/${product.id}`, product)
+  }
+
 }
