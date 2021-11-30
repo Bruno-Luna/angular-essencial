9e7bf3fe38790fb977d3f68ab47c0cc256228076
Bruno Luna <luna.brsilva@gmail.com>
2021-11-29 22:46:34 -0300

    [20] - Rotas e imports

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {HomeComponent} from './views/home/home.component';
 import {ProductCrudComponent} from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "products", component: ProductCrudComponent},
   {path: "products/create", component: ProductCreateComponent},
+  {path: "products/update/:id", component: ProductUpdateComponent},
+  {path: "products/delete/:id", component: ProductDeleteComponent}
 
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -44,6 +46,8 @@ import { MatSortModule } from '@angular/material/sort';
     ProductCreateComponent,
     ProductReadComponent,
     ProductReadModelComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
